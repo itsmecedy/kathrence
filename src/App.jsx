4f6d2a9a448fd0d33cf4e18c626e415d6c4776dc
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-09 23:34:07 +0800

    test edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navigation from "./components/Navigation";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>KathRence</div>
+      <Navigation />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
