2e21ed41ade05b26ec9e0ca5e3be49e9b0011c9b
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-10 16:14:35 +0800

    add react-router and tailwind test and run, no conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Navigation from "./components/Navigation";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <div>
       <Navigation />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <div>Navigation</div>
-  )
+    <div className=" wrapper">
+      <nav className=" flex justify-between">
+        <div>
+          <Link to="/">KathRence</Link>
+        </div>
+        <ul className=" flex flex-row">
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/services">Services</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
