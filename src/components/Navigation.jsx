fc08d02f357f8a62fbede6ae6be3cab85a627955
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-11 23:51:35 +0800

    add testing and save color

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,23 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Navigation() {
   return (
-    <div className=" wrapper">
-      <nav className=" flex justify-between">
-        <div>
-          <Link to="/">KathRence</Link>
+    <div className=" bg-slate-200">
+      <div className="wrapper">
+        <div className=" flex justify-between test" >
+          <div className=" text-2xl ">
+            <Link to="/">KathRence</Link>
+          </div>
+          <ul className=" flex flex-row test">
+            <li className="text-xl test2">
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
         </div>
-        <ul className=" flex flex-row">
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      </div>
     </div>
   );
 }
