3c18e067dd524a13a6b859d708a85d878678e62b
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2024-02-16 00:44:30 +0800

    add carousel

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { Carousel } from "@material-tailwind/react";
 import toy from "../assets/toy.webp";
+import kittyElite from "../assets/kitty-elite.webp";
+import banner from "../assets/asd.png";
 
 export default function Blogs() {
   return (
-    <div className=" wrapper test h-screen">
-      <Carousel transition={{ duration: 1 }}  className="rounded-xl">
-        <img src={toy} alt="image 1" className="h-full w-full object-cover" />
-        <img src={toy} alt="image 2" className="h-full w-full object-cover" />
-        <img src={toy} alt="image 3" className="h-full w-full object-cover" />
-      </Carousel>
+    <div className="wrapper test">
+      <div className="h-80">
+        <Carousel transition={{ duration: 1 }} autoplay={true}>
+          <img
+            src={banner}
+            alt="image 1"
+            className="h-full w-full object-cover"
+          />
+          <img
+            src={banner}
+            alt="image 2"
+            className="h-full w-full object-cover"
+          />
+          <img
+            src={banner}
+            alt="image 3"
+            className="h-full w-full object-cover"
+          />
+        </Carousel>
+      </div>
     </div>
   );
 }
