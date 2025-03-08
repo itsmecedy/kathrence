77d04c84dbbaee1f776d65c6d3e997959498fa22
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2025-03-08 22:55:23 +0800

    add explore with us section in home page

--- src/components/features/NewArrivals.jsx
+++ src/components/features/NewArrivals.jsx
@@ -9,7 +9,12 @@ const NewArrivals = () => {
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {/* Example of a product card */}
         <div className="border p-2 rounded-lg shadow-md flex flex-col items-center">
-          <img src={asd} alt="Product" className="w-full h-40 object-cover" />
+          <img
+            src={asd}
+            alt="Product"
+            className="w-full h-40 object-cover"
+            loading="lazy"
+          />
           <Link
             to="/Products"
             className="text-lg font-semibold mt-2 cursor-pointer underline"
@@ -21,19 +26,7 @@ const NewArrivals = () => {
             <p className="text-gray-800">₱699.00</p>
           </div>
         </div>
-        <div className="border p-2 rounded-lg shadow-md flex flex-col items-center">
-          <img src={asd} alt="Product" className="w-full h-40 object-cover" />
-          <Link
-            to="/Products"
-            className="text-lg font-semibold mt-2 cursor-pointer underline"
-          >
-            Go to About Page
-          </Link>
-          <div className=" flex space-x-2">
-            <p className="text-gray-600 line-through">₱999.00</p>
-            <p className="text-gray-800">₱699.00</p>
-          </div>
-        </div>
+
         {/* Add more product cards here */}
       </div>
     </section>
