65fa91eaa809f44593a7e41f836c4817e2530f9e
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2025-03-10 22:33:20 +0800

    test new arrivals component

--- src/components/features/NewArrivals.jsx
+++ src/components/features/NewArrivals.jsx
@@ -26,6 +26,25 @@ const NewArrivals = () => {
             <p className="text-gray-800">₱699.00</p>
           </div>
         </div>
+        <div className="border p-2 rounded-lg shadow-md flex flex-col items-center">
+          <img
+            src={asd}
+            alt="Product"
+            className="w-full h-40 object-cover"
+            loading="lazy"
+          />
+          <Link
+            to="/Products"
+            className="text-lg font-semibold mt-2 cursor-pointer underline"
+          >
+            Go to About Page
+          </Link>
+          <div className=" flex space-x-2">
+            <p className="text-gray-600 line-through">₱999.00</p>
+            <p className="text-gray-800">₱699.00</p>
+          </div>
+        </div>
+        {/* add more here */}
       </div>
     </section>
   );
