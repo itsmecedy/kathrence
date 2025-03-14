da0a29817ba2152f17cd2ec0fe55dbcafe9f5067
@itsmecedy <91381330+Sidibuy@users.noreply.github.com>
2025-03-14 23:57:23 +0800

    remove red borders

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div className="mt-20 border border-red-300">
+    <div className="mt-20">
       <div className="wrapper mx-auto space-y-10 py-8 text-center lg:max-w-4xl">
         <h2 className="mb-4 text-3xl font-bold">Contact Us</h2>
         <p className="text-lg">
